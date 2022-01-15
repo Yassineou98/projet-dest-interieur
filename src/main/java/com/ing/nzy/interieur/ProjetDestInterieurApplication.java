ed015dceac627647fd156daa7c768caa6d0ec8d4
YASSINE <ouakliyassine98@gmail.com>
2022-01-15 17:11:26 +0100

    initial commit

---
+++ src/main/java/com/ing/nzy/interieur/ProjetDestInterieurApplication.java
@@ -0,0 +1,13 @@
+package com.ing.nzy.interieur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDestInterieurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDestInterieurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/nzy/interieur/ProjetDestInterieurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.nzy.interieur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDestInterieurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
